11b2a128546bea0399328f8b17f2332519709880
Friederik <mr.ilya2003@yandex.ru>
2025-04-30 11:43:32 +0500

    Юзер менеджер, размер чата

--- src/components/UserPicker/UserManager.tsx
+++ src/components/UserPicker/UserManager.tsx
@@ -13,10 +13,10 @@ const UserManager = (props: UserManagerProps) => {
     return (
         <section id="userManager" className={classes.userManager}>
             <UserProfile user={props.currentUser} />
-            <button id="userChangeButton" onClick={props.openUserPicker}>
+            <button id="userChangeButton" style={{borderRadius: '0.4rem'}} onClick={props.openUserPicker}>
                 <img className={globalClasses.icon} src="/assets/new_picker.svg" alt="picker"/>
             </button>
-            <button id="eraseLocalStorageButton" style={{backgroundColor: 'red'}} onClick={props.clearStorage}>
+            <button id="eraseLocalStorageButton" className={classes.resetBtn} style={{borderRadius: '0.4rem'}} onClick={props.clearStorage}>
                 <img className={globalClasses.icon} src="/assets/new_reboot.svg" alt="picker"/>
             </button>
         </section>
